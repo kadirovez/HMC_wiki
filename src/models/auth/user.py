87aa71dd02fceb4b09405b02b68e473149a1e423
faroosha <kadirovezfarhad@gmail.com>
2026-07-09 17:54:10 +0400

    initial commit

---
+++ src/models/auth/user.py
@@ -0,0 +1,90 @@
+
+from datetime import datetime
+
+from sqlalchemy import String, Boolean, DateTime, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.core.settings import settings
+from src.models.base import BaseDataModel
+
+
+class User(BaseDataModel):
+
+    __tablename__ = 'user'
+
+    username: Mapped[str] = mapped_column(
+        String(32),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+
+    first_name: Mapped[str] = mapped_column(
+        String(32),
+        nullable=False,
+        index=True,
+    )
+
+    last_name: Mapped[str] = mapped_column(
+        String(32),
+        nullable=False,
+        index=True,
+    )
+
+    # Email
+    email : Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+
+    email_confirmed: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=False,
+        server_default='false',
+    )
+
+    email_code_limit: Mapped[int] = mapped_column(
+        Integer,
+        default=settings.user_email_code_limit,
+        nullable=False,
+    )
+
+    bad_email_code_time: Mapped[datetime | None] = mapped_column(
+        DateTime,
+        default=None,
+        nullable=True,
+    )
+
+    # Password
+    password : Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
+
+    password_change: Mapped[datetime | None] = mapped_column(
+        DateTime,
+        default=None,
+        nullable=True,
+    )
+
+    bad_password_count: Mapped[int] = mapped_column(
+        Integer,
+        default=0,
+        nullable=False,
+    )
+
+    bad_password_time: Mapped[datetime | None] = mapped_column(
+        DateTime,
+        default=None,
+        nullable=True,
+    )
+
+    main: Mapped["Main | None"] = relationship(
+        'Main',
+        back_populates='user',
+        uselist=False,
+        cascade='all, delete-orphan',
+    )
---
+++ src/utils/get_active_login_sessions.py
@@ -0,0 +1,19 @@
+
+from sqlalchemy import func
+from sqlalchemy.sql import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.auth.login_session import Login
+
+
+async def get_active_login_sessions(db: AsyncSession):
+
+    result = await db.execute(
+        select(func.count()).select_from(Login)
+    )
+
+    active_login_sessions = result.scalar_one()
+    print(f"ACTIVE LOGIN SESSIONS = {active_login_sessions}")
+
+    return active_login_sessions
+
